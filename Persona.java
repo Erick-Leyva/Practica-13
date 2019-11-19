027982a9f0ed248bf0df1fc1dec30004015f09e6
TatoOSBM <42454427+TatoOSBM@users.noreply.github.com>
2019-11-18 20:41:52 -0600

    Clase Persona
    Uso de Builder Pattern e inclusión de una version de toString para ver los atributos de las instancias.

---
+++ Persona.java
@@ -0,0 +1,83 @@
+public class Persona {
+
+	private String nombre;
+	private int edad;
+	private String municipio;
+	private String colegio;
+	private String lugarTrabajo;
+	
+	private Persona() {}
+        
+        public String toString(){
+            return "Nombre: "+this.nombre+
+                    "\nEdad: "+this.edad+
+                    "\nMunicipio: "+this.municipio+
+                    "\nColegio: "+this.colegio+
+                    "\nLugar de Trabajo: "+this.lugarTrabajo;
+        }
+	
+	
+	public static class Builder {
+		private Persona persona;
+		
+		public Builder(String nombre) {
+			persona = new Persona();
+			persona.nombre = nombre;
+		}
+		
+		public Builder setMunicipio (String municipio) {
+			persona.municipio = municipio;
+			return this;
+		}
+		
+		public BuilderMayor setMayor(int edad) {
+			if (edad < 18) throw new IllegalArgumentException("es menor de edad " + edad);
+			persona.edad = edad;
+			return new Persona.BuilderMayor(persona);
+		}
+		
+		public BuilderMenor setMenor(int edad) {
+			if (edad >= 18) throw new IllegalArgumentException("es mayor de edad " + edad);
+			persona.edad = edad;
+			return new Persona.BuilderMenor(persona);
+		}
+	
+	}
+	
+	public static class BuilderMayor {
+		
+		private Persona persona;
+		
+		private BuilderMayor (Persona persona) {
+			this.persona = persona;
+		}
+		
+		public BuilderMayor setLugarTrabajo (String lugarTrabajo) {
+			this.persona.lugarTrabajo = lugarTrabajo;
+			return this;
+		}
+		
+		public Persona build() {
+			return persona;
+		}
+	}
+	
+	public static class BuilderMenor {
+		
+		private Persona persona;
+		
+		private BuilderMenor (Persona persona) {
+			this.persona = persona;
+		}
+		
+		public BuilderMenor setColegio(String colegio) {
+			this.persona.colegio = colegio;
+			return this;
+		}
+		
+		public Persona build() {
+			return persona;
+		}
+	}
+	
+}
